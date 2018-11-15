aac4077d1b4997c341f32460efc6463067ba6ba0
tuhnik <huntlarsen@gmail.com>
2018-11-15 18:24:47 +0200

    Updates

---
+++ precache-manifest.23fd00a86f92d6c116279521f84b3e5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a787cae5943cd673942",
+    "url": "/react-binary-clock/static/js/runtime~main.6a787cae.js"
+  },
+  {
+    "revision": "d370d6ca0aa704a8a5d8",
+    "url": "/react-binary-clock/static/js/main.d370d6ca.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/react-binary-clock/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "d370d6ca0aa704a8a5d8",
+    "url": "/react-binary-clock/static/css/main.8c3e0db4.chunk.css"
+  },
+  {
+    "revision": "a7d92aa55aa85181c9afeb8ce6b637e5",
+    "url": "/react-binary-clock/index.html"
+  }
+];
