9c5647ef5d4d6f37a5c02c131756a4d8458f32be
tuhnik <huntlarsen@gmail.com>
2018-11-18 20:15:55 +0200

    new theme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Brush from './svg/paint-brush.svg'
 import Clock from './svg/clock-o.svg'
 
 class App extends Component {
-  state = { bintime: [], time: [], currentHint: 0, hints:[], blockNumbers: false, theme: 0, themes: ["", "indian", "marine", "france", "typewriter", "crazy", "pacman"]}
+  state = { bintime: [], time: [], currentHint: 0, hints:[], blockNumbers: false, theme: 0, themes: ["", "indian", "marine", "france", "typewriter", "crazy", "pacman", "minimal"]}
 
   componentDidMount = () => {
     this.tick()
